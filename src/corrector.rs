01c44b1be3dbe58d2adc3c8a53a9eb8149224ef6
2gn <101851090+2gn@users.noreply.github.com>
2023-03-22 19:04:18 +0900

    NO FUCKS GIVEN

--- src/corrector.rs
+++
@@ -1,3 +0,0 @@
-pub fn correct(command: String) -> String {
-    command.replace("cs", "cd")
-}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
-mod corrector;
+use std::env;
+// use std::str;
+
+fn is_perfect_command(command: &str) -> Result<(), ()> {
+    if command == "ls" {
+        Ok(())
+    } else {
+        Err(())
+    }
+}
 
 fn main() {
-    println!(
-        "{}", 
-        corrector::correct("cs ~/Documents".to_string())
-    )
+    if let Ok(shell_history) = env::var("TF_HISTORY") {
+        let shell_history = shell_history.split("\n").collect::<Vec<&str>>()[0];
+        match is_perfect_command(shell_history) {
+            Ok(_) => {
+                println!("No fucks given");
+            },
+            Err(_) => {
+                println!("Problems");
+            }
+        }
+
+    } else {
+        println!("environment variable TF_HISTORY is not set");
+    }
 }
